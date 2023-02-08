61fbbecc6e4229c21d1047fad53a357182ee820e
zh92 <zhehao92@gmail.com>
2023-02-08 09:52:54 +0800

    Initial commit

---
+++ src/main/java/sg/edu/nus/iss/day13_lecture/Day13LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day13_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day13LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day13LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day13_lecture/Day13LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day13_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
